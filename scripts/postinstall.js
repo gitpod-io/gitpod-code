7a201b41508858105fa3896534efb8a5cf0133aa
Filip Troníček <filip.tronicek@seznam.cz>
2023-01-27 17:17:56 +0100

    Disable `yarn` workspaces

---
+++ scripts/postinstall.js
@@ -0,0 +1,37 @@
+const cp = require('child_process');
+const yarn = 'yarn';
+const glob = require('glob');
+
+/**
+ * @param {string} location
+ * @param {*} [opts]
+ */
+function yarnInstall(location, opts) {
+	opts = opts || { env: process.env };
+	opts.cwd = location;
+	opts.stdio = 'inherit';
+
+	const raw = process.env['npm_config_argv'] || '{}';
+	const argv = JSON.parse(raw);
+	const original = argv.original || [];
+	if (opts.ignoreEngines) {
+		args.push('--ignore-engines');
+		delete opts.ignoreEngines;
+	}
+
+	console.log(`Installing dependencies in ${location}...`);
+	console.log(`$ yarn ${args.join(' ')}`);
+	const result = cp.spawnSync(yarn, args, opts);
+
+	if (result.error || result.status !== 0) {
+		process.exit(1);
+	}
+}
+
+const extensionDirectories = glob.sync("./*/package.json")
+
+for (let dir of extensionDirectories) {
+    dir = dir.replace("package.json", "");
+    yarnInstall(dir, { ignoreEngines: true });
+}
+
