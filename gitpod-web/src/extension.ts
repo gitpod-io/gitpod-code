6521e4f708b12df09c95cbb97bfe92f6d5903e8c
Jean Pierre <jeanp413@hotmail.com>
2023-09-05 13:12:22 +0000

    Start migration due to keytar removal in the future Some refactor

--- gitpod-web/src/extension.ts
+++ gitpod-web/src/extension.ts
@@ -4,12 +4,11 @@
 
 import { GitpodExtensionContext, setupGitpodContext, registerTasks, registerIpcHookCli, ExposedServedGitpodWorkspacePort, GitpodWorkspacePort, isExposedServedGitpodWorkspacePort } from 'gitpod-shared';
 import { PortsStatus, PortVisibility } from '@gitpod/supervisor-api-grpc/lib/status_pb';
-import type * as keytarType from 'keytar';
-import fetch from 'node-fetch';
 import * as vscode from 'vscode';
 import { registerWelcomeWalkthroughContribution, WELCOME_WALKTROUGH_KEY } from './welcomeWalktrough';
 import { GitpodPortViewProvider } from './portViewProvider';
 import { registerExtensionManagement } from './extensionManagement';
+import { GithubAuthenticationProvider, GitpodAuthenticationProvider } from './authentication';
 
 let gitpodContext: GitpodExtensionContext | undefined;
 export async function activate(context: vscode.ExtensionContext) {
@@ -22,9 +21,12 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	context.globalState.setKeysForSync([WELCOME_WALKTROUGH_KEY]);
 
+	gitpodContext.subscriptions.push(new GitpodAuthenticationProvider(gitpodContext));
+	gitpodContext.subscriptions.push(new GithubAuthenticationProvider(gitpodContext));
+
+
 	registerCommands(gitpodContext);
 	registerDesktop();
-	registerAuth(gitpodContext);
 	registerPorts(gitpodContext);
 	registerTasks(gitpodContext).then(() => {
 		setTimeout(() => {
@@ -44,180 +46,6 @@ export async function activate(context: vscode.ExtensionContext) {
 
 export async function deactivate() {
 	await gitpodContext?.dispose();
-}
-
-function registerAuth(context: GitpodExtensionContext): void {
-	type Keytar = {
-		getPassword: typeof keytarType['getPassword'];
-		setPassword: typeof keytarType['setPassword'];
-		deletePassword: typeof keytarType['deletePassword'];
-	};
-	interface SessionData {
-		id: string;
-		account?: {
-			label?: string;
-			displayName?: string;
-			id: string;
-		};
-		scopes: string[];
-		accessToken: string;
-	}
-	interface UserInfo {
-		id: string;
-		accountName: string;
-	}
-	async function resolveAuthenticationSession(data: SessionData, resolveUser: (data: SessionData) => Promise<UserInfo>): Promise<vscode.AuthenticationSession> {
-		const needsUserInfo = !data.account;
-		const userInfo = needsUserInfo ? await resolveUser(data) : undefined;
-		return {
-			id: data.id,
-			account: {
-				label: data.account
-					? data.account.label || data.account.displayName!
-					: userInfo!.accountName,
-				id: data.account?.id ?? userInfo!.id
-			},
-			scopes: data.scopes,
-			accessToken: data.accessToken
-		};
-	}
-	function hasScopes(session: vscode.AuthenticationSession, scopes?: readonly string[]): boolean {
-		return !scopes || scopes.every(scope => session.scopes.indexOf(scope) !== -1);
-	}
-	//#endregion
-
-	//#region gitpod auth
-	context.pendingActivate.push((async () => {
-		const sessions: vscode.AuthenticationSession[] = [];
-		const onDidChangeSessionsEmitter = new vscode.EventEmitter<vscode.AuthenticationProviderAuthenticationSessionsChangeEvent>();
-		try {
-			const resolveGitpodUser = async () => {
-				const owner = await context.owner;
-				return {
-					id: owner.id,
-					accountName: owner.name!
-				};
-			};
-			if (vscode.env.uiKind === vscode.UIKind.Web) {
-				const keytar: Keytar = require('keytar');
-				const value = await keytar.getPassword(`${vscode.env.uriScheme}-gitpod.login`, 'account');
-				if (value) {
-					await keytar.deletePassword(`${vscode.env.uriScheme}-gitpod.login`, 'account');
-					const sessionData: SessionData[] = JSON.parse(value);
-					if (sessionData.length) {
-						const session = await resolveAuthenticationSession(sessionData[0], resolveGitpodUser);
-						sessions.push(session);
-					}
-				}
-			} else {
-				const resp = await context.supervisor.getToken(
-					'gitpod',
-					context.info.gitpodApi!.host,
-					['function:accessCodeSyncStorage']
-				);
-				const session = await resolveAuthenticationSession({
-					// current session ID should remain stable between window reloads
-					// otherwise setting sync will log out
-					id: 'gitpod-current-session',
-					accessToken: resp.token,
-					scopes: resp.scopeList
-				}, resolveGitpodUser);
-				sessions.push(session);
-				onDidChangeSessionsEmitter.fire({ added: [session], changed: [], removed: [] });
-			}
-		} catch (e) {
-			console.error('Failed to restore Gitpod session:', e);
-		}
-		context.subscriptions.push(onDidChangeSessionsEmitter);
-		context.subscriptions.push(vscode.authentication.registerAuthenticationProvider('gitpod', 'Gitpod', {
-			onDidChangeSessions: onDidChangeSessionsEmitter.event,
-			getSessions: scopes => {
-				if (!scopes) {
-					return Promise.resolve(sessions);
-				}
-				return Promise.resolve(sessions.filter(session => hasScopes(session, scopes)));
-			},
-			createSession: async () => {
-				throw new Error('not supported');
-			},
-			removeSession: async () => {
-				throw new Error('not supported');
-			},
-		}, { supportsMultipleAccounts: false }));
-	})());
-	//#endregion gitpod auth
-
-	//#region github auth
-	context.pendingActivate.push((async () => {
-		const onDidChangeGitHubSessionsEmitter = new vscode.EventEmitter<vscode.AuthenticationProviderAuthenticationSessionsChangeEvent>();
-		const gitHubSessionID = 'github-session';
-		let gitHubSession: vscode.AuthenticationSession | undefined;
-
-		async function resolveGitHubUser(data: SessionData): Promise<UserInfo> {
-			const userResponse = await fetch('https://api.github.com/user', {
-				headers: {
-					Authorization: `token ${data.accessToken}`,
-					'User-Agent': 'Gitpod-Code'
-				}
-			});
-			if (!userResponse.ok) {
-				throw new Error(`Getting GitHub account info failed: ${userResponse.statusText}`);
-			}
-			const user = await (userResponse.json() as Promise<{ id: string; login: string }>);
-			return {
-				id: user.id,
-				accountName: user.login
-			};
-		}
-
-		async function loginGitHub(scopes: string[]): Promise<vscode.AuthenticationSession> {
-			const resp = await context.supervisor.getToken(
-				'git',
-				'github.com',
-				scopes
-			);
-			gitHubSession = await resolveAuthenticationSession({
-				id: gitHubSessionID,
-				accessToken: resp.token,
-				scopes: resp.scopeList
-			}, resolveGitHubUser);
-			onDidChangeGitHubSessionsEmitter.fire({ added: [gitHubSession], changed: [], removed: [] });
-			return gitHubSession;
-		}
-
-		try {
-			await loginGitHub([]);
-		} catch (e) {
-			console.error('Failed an initial GitHub login:', e);
-		}
-
-		context.subscriptions.push(vscode.authentication.registerAuthenticationProvider('github', 'GitHub', {
-			onDidChangeSessions: onDidChangeGitHubSessionsEmitter.event,
-			getSessions: scopes => {
-				const sessions = [];
-				if (gitHubSession && hasScopes(gitHubSession, scopes)) {
-					sessions.push(gitHubSession);
-				}
-				return Promise.resolve(sessions);
-			},
-			createSession: async scopes => {
-				try {
-					const session = await loginGitHub(scopes.slice());
-					return session;
-				} catch (e) {
-					console.error('GitHub sign in failed: ', e);
-					throw e;
-				}
-			},
-			removeSession: async id => {
-				if (id === gitHubSession?.id) {
-					const session = gitHubSession;
-					gitHubSession = undefined;
-					onDidChangeGitHubSessionsEmitter.fire({ removed: [session], added: [], changed: [] });
-				}
-			},
-		}, { supportsMultipleAccounts: false }));
-	})());
 }
 
 async function registerPorts(context: GitpodExtensionContext): Promise<void> {
---
+++ gitpod-web/src/util/keychain.ts
@@ -0,0 +1,48 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Gitpod. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { ILogService} from 'gitpod-shared';
+
+export default class Keychain {
+	constructor(
+		private readonly context: vscode.ExtensionContext,
+		private readonly serviceId: string,
+		private readonly logger: ILogService
+	) { }
+
+	async setToken(token: string): Promise<void> {
+		try {
+			return await this.context.secrets.store(this.serviceId, token);
+		} catch (e) {
+			// Ignore
+			this.logger.error(`Setting token failed: ${e}`);
+		}
+	}
+
+	async getToken(): Promise<string | null | undefined> {
+		try {
+			const secret = await this.context.secrets.get(this.serviceId);
+			if (secret && secret !== '[]') {
+				this.logger.trace('Token acquired from secret storage.');
+			}
+			return secret;
+		} catch (e) {
+			// Ignore
+			this.logger.error(`Getting token failed: ${e}`);
+			return Promise.resolve(undefined);
+		}
+	}
+
+	async deleteToken(): Promise<void> {
+		try {
+			return await this.context.secrets.delete(this.serviceId);
+		} catch (e) {
+			// Ignore
+			this.logger.error(`Deleting token failed: ${e}`);
+			return Promise.resolve(undefined);
+		}
+	}
+}
